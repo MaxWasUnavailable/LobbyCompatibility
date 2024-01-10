633af97f68dd1cf78a6c39be819008a399a9f0a3
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-01-10 03:19:34 +0100

    Add CompatibilityLevel enum

---
+++ LobbyCompatibility/Enums/CompatibilityLevel.cs
@@ -0,0 +1,33 @@
+namespace LobbyCompatibility.Enums;
+
+/// <summary>
+///     Specifies the compatibility level of the plugin.
+/// </summary>
+public enum CompatibilityLevel
+{
+    /// <summary>
+    ///     Mod only impacts the client.
+    ///     Mod is not checked at all, VersionStrictness does not apply.
+    /// </summary>
+    ClientOnly = 0,
+
+    /// <summary>
+    ///     Mod only impacts the server, and might implicitly impact the client without the client needing to have it installed
+    ///     for it to work.
+    ///     Mod is only required by the server. VersionStrictness only applies if the mod is installed on the client.
+    /// </summary>
+    ServerOnly = 1,
+
+    /// <summary>
+    ///     Mod impacts both the client and the server, and adds functionality that requires the mod to be installed on both.
+    ///     Mod must be loaded on server and client. Version checking depends on the VersionStrictness.
+    /// </summary>
+    Everyone = 2,
+
+    /// <summary>
+    ///     Not every client needs to have the mod installed, but if it is installed, the server also needs to have it.
+    ///     Generally used for mods that add extra (optional) functionality to the client if the server has it installed.
+    ///     Mod must be loaded on server. Version checking depends on the VersionStrictness.
+    /// </summary>
+    ClientOptional = 3
+}
