3050b80302424887bb77473ba92b7b64101a6346
Xilophor <ryanzgreg@gmail.com>
2024-01-10 16:04:02 -0500

    Change async method finder location; added more transpiler patches

--- LobbyCompatibility/Patches/LeaderboardPatch.cs
+++ LobbyCompatibility/Patches/LeaderboardPatch.cs
@@ -1,36 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Reflection.Emit;
-using System.Runtime.CompilerServices;
 using HarmonyLib;
 
 namespace LobbyCompatibility.Patches;
 
+[HarmonyPatch]
 public class LeaderboardPatch
 {
-    
-    internal static IEnumerable<CodeInstruction> ModdedLeaderboards(IEnumerable<CodeInstruction> instructions)
+    [HarmonyPatch(typeof(MenuManager))]
+    [HarmonyPatch(nameof(MenuManager.SetIfChallengeMoonHasBeenCompleted))]
+    [HarmonyPatch(nameof(MenuManager.EnableLeaderboardDisplay))]
+    [HarmonyPatch(nameof(MenuManager.EnableLeaderboardDisplay))]
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> ChangeSaveFile(IEnumerable<CodeInstruction> instructions)
     {
-#if DEBUG
-        LobbyCompatibilityPlugin.Logger?.LogDebug("-----START-----");
-        instructions.Do(inst => LobbyCompatibilityPlugin.Logger?.LogDebug(inst));
-        LobbyCompatibilityPlugin.Logger?.LogDebug("-----END-----");
-#endif
-        
-        var newInstructions = new CodeMatcher(instructions)
+        // For all instructions, do this: (for each)
+        return instructions.Select(codeInstruction =>
+            {
+                // If the operand is "LCChallengeFile", replace it with "LCModdedChallengeFile", otherwise leave as is
+                codeInstruction.operand = codeInstruction.Is(OpCodes.Ldstr, "LCChallengeFile")
+                    ? "LCModdedChallengeFile"
+                    : codeInstruction.operand;
+                return codeInstruction;
+            }
+        );
+    }
+    
+    internal static IEnumerable<CodeInstruction> ChangeToModdedLeaderboard(IEnumerable<CodeInstruction> instructions)
+    {
+        // Create CodeMatcher from instructions with all instances of "LCChallengeFile" replaced
+        return new CodeMatcher(ChangeSaveFile(instructions))
+            // Find the instance of "challenge{0}"
             .SearchForward(i => i.Is(OpCodes.Ldstr, "challenge{0}"))
+            // Replace the leaderboard name operand with the modded version
             .SetOperandAndAdvance("modded_challenge{0}")
-            .InstructionEnumeration()
-            .ToArray();
-
-#if DEBUG
-        LobbyCompatibilityPlugin.Logger?.LogDebug("-----START-----");
-        newInstructions.Do(inst => LobbyCompatibilityPlugin.Logger?.LogDebug(inst));
-        LobbyCompatibilityPlugin.Logger?.LogDebug("-----END-----");
-#endif
-        
-        return newInstructions;
+            // Turn CodeMatcher to IEnumerable<CodeInstruction> so it can be returned
+            .InstructionEnumeration();
     }
 }
