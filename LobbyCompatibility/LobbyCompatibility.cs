8923179140b2cccd4e8f62653e02e0dc17e4ba50
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-01-10 02:47:51 +0100

    Initial commit

---
+++ LobbyCompatibility/LobbyCompatibility.cs
@@ -0,0 +1,63 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace LobbyCompatibility;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class LobbyCompatibility : BaseUnityPlugin
+{
+    private bool _isPatched;
+    private Harmony? Harmony { get; set; }
+    private new static ManualLogSource? Logger { get; set; }
+    public static LobbyCompatibility? Instance { get; private set; }
+    
+    private void Awake()
+    {
+        // Set instance
+        Instance = this;
+
+        // Init logger
+        Logger = base.Logger;
+        
+        // Patch using Harmony
+        PatchAll();
+        
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    public void PatchAll()
+    {
+        if (_isPatched)
+        {
+            Logger?.LogWarning("Already patched!");
+            return;
+        }
+
+        Logger?.LogDebug("Patching...");
+
+        Harmony ??= new Harmony(PluginInfo.PLUGIN_GUID);
+        
+        Harmony.PatchAll();
+        _isPatched = true;
+
+        Logger?.LogDebug("Patched!");
+    }
+
+    public void UnpatchAll()
+    {
+        if (!_isPatched)
+        {
+            Logger?.LogWarning("Already unpatched!");
+            return;
+        }
+
+        Logger?.LogDebug("Unpatching...");
+
+        Harmony!.UnpatchSelf();
+        _isPatched = false;
+
+        Logger?.LogDebug("Unpatched!");
+    }
+}
