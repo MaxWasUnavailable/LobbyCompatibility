00a0c3271be9389892f62c0f17f67ff7730b7466
Xilophor <ryanzgreg@gmail.com>
2024-01-11 18:46:45 -0500

    Shortened Leaderboard Patchfile
    Done so by publicizing game assets from GameLibs

--- LobbyCompatibility/Patches/LeaderboardNamePatch.cs
+++ LobbyCompatibility/Patches/LeaderboardNamePatch.cs
@@ -9,12 +9,7 @@ namespace LobbyCompatibility.Patches;
 [HarmonyWrapSafe]
 internal class LeaderboardNamePatch
 {
-    [HarmonyTargetMethod]
-    private static MethodBase TargetMethod()
-    {
-        return typeof(SteamUserStats).Assembly.GetType("Steamworks.ISteamUserStats")!.GetMethod("FindOrCreateLeaderboard", (BindingFlags)60)!;
-    }
-    
+    [HarmonyPatch(nameof(ISteamUserStats), nameof(ISteamUserStats.FindOrCreateLeaderboard))]
     [HarmonyPrefix]
     private static void SavePrefix(ref string pchLeaderboardName)
     {
