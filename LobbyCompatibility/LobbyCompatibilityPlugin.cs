33afcf6b49e5b1e55f3f1d24c3f8f4338a1f28c3
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-01-25 01:45:00 +0100

    Remove unused method

--- LobbyCompatibility/LobbyCompatibilityPlugin.cs
+++ LobbyCompatibility/LobbyCompatibilityPlugin.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
-using LobbyCompatibility.Patches;
 
 namespace LobbyCompatibility;
 
@@ -15,7 +11,7 @@ public class LobbyCompatibilityPlugin : BaseUnityPlugin
     private Harmony? Harmony { get; set; }
     internal new static ManualLogSource? Logger { get; set; }
     public static LobbyCompatibilityPlugin? Instance { get; private set; }
-    
+
     private void Awake()
     {
         // Set instance
@@ -23,31 +19,12 @@ public class LobbyCompatibilityPlugin : BaseUnityPlugin
 
         // Init logger
         Logger = base.Logger;
-        
+
         // Patch using Harmony
         PatchAll();
-        
+
         // Plugin startup logic
         Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-    }
-    
-    
-    /// <summary>
-    /// Retrieves the MethodInfo of the compiler-generated async method.
-    /// Async method content is not actually in the <c>async Method()</c>, but instead is in a separate <c>struct</c> under the method "MoveNext";
-    /// this function retrieves that method info.
-    /// </summary>
-    /// <param name="type">(<see cref="Type"/>) The type of the class housing the method.</param>
-    /// <param name="method">(<see cref="string"/>) The name of the method being patched.</param>
-    /// <returns>(<see cref="MethodInfo"/>) The info of the async "MoveNext" method.</returns>
-    public static MethodInfo? GetAsyncInfo(Type type, string method)
-    {
-        // Get the Method Info of the target Async Method
-        return AccessTools.Method(type, method)
-            // Find the AsyncStateMachine class from target method
-            .GetCustomAttribute<AsyncStateMachineAttribute>()
-            // Get the struct type (random compiler junk)
-            .StateMachineType.GetMethod("MoveNext", (BindingFlags)60);
     }
 
     public void PatchAll()
@@ -61,7 +38,7 @@ public class LobbyCompatibilityPlugin : BaseUnityPlugin
         Logger?.LogDebug("Patching...");
 
         Harmony ??= new Harmony(PluginInfo.PLUGIN_GUID);
-        
+
         Harmony.PatchAll();
         _isPatched = true;
 
