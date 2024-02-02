f343ece1f0e7f27a0c3218c535471f377dde82d4
legoandmars <legoandmars@gmail.com>
2024-02-01 20:46:28 -0800

    Document dropdown class slightly better

--- LobbyCompatibility/Behaviours/ModdedLobbyFilterDropdown.cs
+++ LobbyCompatibility/Behaviours/ModdedLobbyFilterDropdown.cs
@@ -9,26 +9,46 @@ namespace LobbyCompatibility.Behaviours
     /// </summary>
     internal class ModdedLobbyFilterDropdown : MonoBehaviour
     {
-        public ModdedLobbyFilter LobbyFilter;
+        public ModdedLobbyFilter LobbyFilter { get; private set; }
         private TMP_Dropdown? _dropdown; 
 
         public void Awake()
         {
-            LobbyFilter = LobbyCompatibilityPlugin.Config?.DefaultModdedLobbyFilter.Value ?? ModdedLobbyFilter.CompatibleFirst;
+            // Set default to whatever the user has configured
+            UpdateLobbyFilter(LobbyCompatibilityPlugin.Config?.DefaultModdedLobbyFilter.Value ?? ModdedLobbyFilter.CompatibleFirst);
 
             if (_dropdown != null)
                 _dropdown.SetValueWithoutNotify((int)LobbyFilter);
         }
 
+        /// <summary>
+        ///     Registers the <see cref="TMP_Dropdown"/>. Required for syncing the default config with UI.
+        /// </summary>
+        /// <param name="dropdown"> The dropdown. </param>
         public void SetDropdown(TMP_Dropdown dropdown)
         {
             _dropdown = dropdown;
         }
 
-        public void ChangeFilterType(int newValue)
+        /// <summary>
+        ///     Called automatically when the <see cref="TMP_Dropdown"/> value is updated.
+        /// </summary>
+        /// <param name="index"> The index of the new filter type. </param>
+        public void ChangeFilterType(int index)
         {
-            LobbyFilter = (ModdedLobbyFilter)newValue;
-            LobbyCompatibilityPlugin.Logger?.LogInfo(LobbyFilter);
+            UpdateLobbyFilter((ModdedLobbyFilter)index);
+        }
+
+        /// <summary>
+        ///     Updates the <see cref="ModdedLobbyFilter"/> internally.
+        ///     This will be called when the user changes the value manually, and when the menu scene is opened/reopened.
+        /// </summary>
+        /// <param name="lobbyFilter"> The new <see cref="ModdedLobbyFilter"/> to use. </param>
+        private void UpdateLobbyFilter(ModdedLobbyFilter lobbyFilter)
+        {
+            LobbyFilter = lobbyFilter;
+
+            // TODO: Hook this up. Use a singleton or action or whatever
         }
     }
 }
