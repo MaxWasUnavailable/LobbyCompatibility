13a3d5c91a048a3aae4d9525b004a58d6c8cd235
Bobbie <34404266+legoandmars@users.noreply.github.com>
2024-02-04 05:04:19 -0800

    Make ModdedLobbyFilterDropdown namespace consistent

--- LobbyCompatibility/Behaviours/ModdedLobbyFilterDropdown.cs
+++ LobbyCompatibility/Behaviours/ModdedLobbyFilterDropdown.cs
@@ -2,59 +2,58 @@
 using TMPro;
 using UnityEngine;
 
-namespace LobbyCompatibility.Behaviours
+namespace LobbyCompatibility.Behaviours;
+
+/// <summary>
+///     Used to handle any changes to the <see cref="ModdedLobbyFilter"/> when searching for lobbies.
+/// </summary>
+internal class ModdedLobbyFilterDropdown : MonoBehaviour
 {
-    /// <summary>
-    ///     Used to handle any changes to the <see cref="ModdedLobbyFilter"/> when searching for lobbies.
-    /// </summary>
-    internal class ModdedLobbyFilterDropdown : MonoBehaviour
+    public ModdedLobbyFilter LobbyFilter { get; private set; }
+    public static ModdedLobbyFilterDropdown? Instance { get; private set; }
+    private TMP_Dropdown? _dropdown;
+    private SteamLobbyManager? _steamLobbyManager;
+
+    public void Awake()
     {
-        public ModdedLobbyFilter LobbyFilter { get; private set; }
-        public static ModdedLobbyFilterDropdown? Instance { get; private set; }
-        private TMP_Dropdown? _dropdown;
-        private SteamLobbyManager? _steamLobbyManager;
+        // Set default to whatever the user has configured
+        UpdateLobbyFilter(LobbyCompatibilityPlugin.Config?.DefaultModdedLobbyFilter.Value ?? ModdedLobbyFilter.CompatibleFirst);
 
-        public void Awake()
-        {
-            // Set default to whatever the user has configured
-            UpdateLobbyFilter(LobbyCompatibilityPlugin.Config?.DefaultModdedLobbyFilter.Value ?? ModdedLobbyFilter.CompatibleFirst);
+        if (_dropdown != null)
+            _dropdown.SetValueWithoutNotify((int)LobbyFilter);
 
-            if (_dropdown != null)
-                _dropdown.SetValueWithoutNotify((int)LobbyFilter);
+        Instance = this;
+        _steamLobbyManager = FindObjectOfType<SteamLobbyManager>();
+    }
 
-            Instance = this;
-            _steamLobbyManager = FindObjectOfType<SteamLobbyManager>();
-        }
+    /// <summary>
+    ///     Registers the <see cref="TMP_Dropdown"/>. Required for syncing the default config with UI.
+    /// </summary>
+    /// <param name="dropdown"> The dropdown. </param>
+    public void SetDropdown(TMP_Dropdown dropdown)
+    {
+        _dropdown = dropdown;
+    }
 
-        /// <summary>
-        ///     Registers the <see cref="TMP_Dropdown"/>. Required for syncing the default config with UI.
-        /// </summary>
-        /// <param name="dropdown"> The dropdown. </param>
-        public void SetDropdown(TMP_Dropdown dropdown)
-        {
-            _dropdown = dropdown;
-        }
+    /// <summary>
+    ///     Called automatically when the <see cref="TMP_Dropdown"/> value is updated.
+    /// </summary>
+    /// <param name="index"> The index of the new filter type. </param>
+    public void ChangeFilterType(int index)
+    {
+        UpdateLobbyFilter((ModdedLobbyFilter)index);
+        
+        if (_steamLobbyManager != null)
+            _steamLobbyManager.RefreshServerListButton();
+    }
 
-        /// <summary>
-        ///     Called automatically when the <see cref="TMP_Dropdown"/> value is updated.
-        /// </summary>
-        /// <param name="index"> The index of the new filter type. </param>
-        public void ChangeFilterType(int index)
-        {
-            UpdateLobbyFilter((ModdedLobbyFilter)index);
-            
-            if (_steamLobbyManager != null)
-                _steamLobbyManager.RefreshServerListButton();
-        }
-
-        /// <summary>
-        ///     Updates the <see cref="ModdedLobbyFilter"/> internally.
-        ///     This will be called when the user changes the value manually, and when the menu scene is opened/reopened.
-        /// </summary>
-        /// <param name="lobbyFilter"> The new <see cref="ModdedLobbyFilter"/> to use. </param>
-        private void UpdateLobbyFilter(ModdedLobbyFilter lobbyFilter)
-        {
-            LobbyFilter = lobbyFilter;
-        }
+    /// <summary>
+    ///     Updates the <see cref="ModdedLobbyFilter"/> internally.
+    ///     This will be called when the user changes the value manually, and when the menu scene is opened/reopened.
+    /// </summary>
+    /// <param name="lobbyFilter"> The new <see cref="ModdedLobbyFilter"/> to use. </param>
+    private void UpdateLobbyFilter(ModdedLobbyFilter lobbyFilter)
+    {
+        LobbyFilter = lobbyFilter;
     }
 }
