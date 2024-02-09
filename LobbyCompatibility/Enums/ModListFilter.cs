a4408d43e74e7e93272d701da3ecaa50e9c8e53d
legoandmars <legoandmars@gmail.com>
2024-02-09 03:27:50 -0800

    filtering enum

---
+++ LobbyCompatibility/Enums/ModListFilter.cs
@@ -0,0 +1,30 @@
+﻿namespace LobbyCompatibility.Enums;
+
+/// <summary>
+///     Specifies what PluginDiff types to show in the ModListPanel.
+/// </summary>
+public enum ModListFilter
+{
+    /// <summary>
+    ///     ModList shows every plugin, regardless of its PluginDiffResult.
+    /// </summary>
+    All,
+
+    /// <summary>
+    ///     ModList only shows plugins with the following PluginDiffResult:
+    ///     Compatible.
+    /// </summary>
+    Compatible,
+
+    /// <summary>
+    ///     ModList only shows plugins with the following PluginDiffResult:
+    ///     ServerMissingMod, ClientMissingMod, ModVersionMismatch.
+    /// </summary>
+    Incompatible,
+
+    /// <summary>
+    ///     ModList only shows plugins with the following PluginDiffResult:
+    ///     Unknown.
+    /// </summary>
+    Unknown
+}
