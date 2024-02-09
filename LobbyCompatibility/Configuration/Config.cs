7cb531e64cedfab9b31854d5113f60bb277a024f
legoandmars <legoandmars@gmail.com>
2024-02-09 03:32:25 -0800

    Add ModListFilter to config

--- LobbyCompatibility/Configuration/Config.cs
+++ LobbyCompatibility/Configuration/Config.cs
@@ -13,11 +13,20 @@ public class Config
     /// </summary>
     public ConfigEntry<ModdedLobbyFilter> DefaultModdedLobbyFilter;
 
+    /// <summary>
+    ///     Default <see cref="ModListFilter"/> when viewing a lobby's mod list.
+    /// </summary>
+    public ConfigEntry<ModListFilter> DefaultModListTab;
+
     public Config(ConfigFile configFile)
     {
         DefaultModdedLobbyFilter = configFile.Bind("General",
             "Default Lobby Filter Type",
             ModdedLobbyFilter.CompatibleFirst,
             "The default filter to apply when searching for a public lobby");
+        DefaultModListTab = configFile.Bind("General",
+            "Default ModList Tab",
+            ModListFilter.All,
+            "The default tab to use when viewing a lobby's mod list.");
     }
 }
